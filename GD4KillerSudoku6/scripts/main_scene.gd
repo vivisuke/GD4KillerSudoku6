fe49de8e6e03d0e50c60c83d47d56ce393678ec1
vivisuke <ntsuda@master.email.ne.jp>
2023-04-04 17:12:08 +0900

    0013 ◎ C 04/04 04/04 0.001│ToDo│解答生成

--- GD4KillerSudoku6/scripts/main_scene.gd
+++ GD4KillerSudoku6/scripts/main_scene.gd
@@ -90,15 +90,97 @@ const CAGE_TABLE = [
 	],
 ]
 
+var qix                 	# 問題番号 [0, N]
+var qID                 	# 問題ID
+var qSolved = false     	# 現問題をクリア済みか？
+#var qSolvedStat = false     # 現問題をクリア状態か？
+#var elapsedTime = 0.0   	# 経過時間（単位：秒）
+var symmetric = true		# 対称形問題
+var qCreating = false		# 問題生成中
+var solvedStat = false		# クリア済み状態
+var paused = false			# ポーズ状態
+var sound = true			# 効果音
+var menuPopuped = false
+var hint_showed = false
+var memo_mode = false		# メモ（候補数字）エディットモード
+var in_button_pressed = false	# ボタン押下処理中
+var hint_ix = -1			# ヒントを入れる箇所
+var hint_count_down = 0.0	# 0.0より大きい：ヒント表示カウントダウン中
+var confetti_count_down = 0.0	# 0.0より大きい：紙吹雪表示中
+var hint_next_pos			# 次ボタン位置
+var hint_next_pos0			# 次ボタン初期位置
+var hint_next_vy			# 次ボタン速度
+var saved_cell_data = []
+
+#var hint_next_scale = 1.0	# ヒント次ボタン表示スケール
+#var hint_num				# ヒントで確定する数字、[1, 9]
+var hint_numstr				# ヒントで確定する数字、[1, 9]
+#var hint_ix = 0				# 0, 1, 2, ...
+var hint_texts = []			# ヒントテキスト配列
+#var restarted = false
+var saved_time
+var nEmpty = 0				# 空欄数
+var nDuplicated = 0			# 重複、合計不正数字数
+#var optGrade = -1			# 問題グレード、0: 入門、1:初級、2:ノーマル（初中級）
+var diffculty = 0			# 難易度、フルハウス: 1, 隠れたシングル: 2, 裸のシングル: 10pnt？
+var num_buttons = []		# 各数字ボタンリスト [0] -> 削除ボタン、[1] -> Button1, ...
+var num_used = []			# 各数字使用数（手がかり数字＋入力数字）
+var cur_num = -1			# 選択されている数字ボタン、-1 for 選択無し
+var cur_cell_ix = -1		# 選択されているセルインデックス、-1 for 選択無し
+var input_num = 0			# 入力された数字
+var nRemoved
+var nAnswer = 0				# 解答数
+
 var cage_labels = []		# ケージ合計数字用ラベル配列
 var clue_labels = []		# 手がかり数字用ラベル配列
 var input_labels = []		# 入力数字用ラベル配列
+var ans_num = []			# 解答の各セル数値、1～N_HORZ
+#var ans_bit = []			# 解答の各セル数値（0 | BIT_1 | BIT_2 | ... | BIT_9）
+var cell_bit = []			# 各セル数値（0 | BIT_1 | BIT_2 | ... | BIT_9）
+var quest_cages = []		# クエストケージリスト配列、要素：[sum, ix1, ix2, ...]
+var cage_list = []			# ケージリスト配列、要素：IX_CAGE_XXX
+var cage_ix = []			# 各セルのケージリスト配列インデックス
+var candidates_bit = []		# 入力可能ビット論理和
+var line_used = []			# 各行の使用済みビット
+var column_used = []		# 各カラムの使用済みビット
+var box_used = []			# 各3x3ブロックの使用済みビット
+var memo_labels = []		# メモ（候補数字）用ラベル配列（２次元）
+var memo_text = []			# ポーズ復活時用ラベルテキスト配列（２次元）
+var shock_wave_timer = -1
+var undo_ix = 0
+var undo_stack = []			# 要素：[ix old new]、old, new は 0～9 の数値、0 for 空欄
+
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	init_labels()
+	#
+	cell_bit.resize(N_CELLS)
+	ans_num.resize(N_CELLS)
+	#ans_bit.resize(N_CELLS)
+	candidates_bit.resize(N_CELLS)
+	cage_ix.resize(N_CELLS)
+	line_used.resize(N_HORZ)
+	column_used.resize(N_HORZ)
+	box_used.resize(N_HORZ)
+	memo_text.resize(N_CELLS)
+	num_used.resize(N_HORZ + 1)		# +1 for 0
+	#
+	gen_ans()
 	pass # Replace with function body.
 
+func xyToIX(x, y) -> int: return x + y * N_HORZ
+func num_to_bit(n : int): return 1 << (n-1) if n > 0 else 0
+func bit_to_num(b):
+	var mask = 1
+	for i in range(N_HORZ):
+		if (b & mask) != 0: return i + 1
+		mask <<= 1
+	return 0
+func bit_to_numstr(b):
+	if b == 0: return ""
+	return "%d" % bit_to_num(b)
 func init_labels():
 	# 手がかり数字、入力数字用 Label 生成
 	for y in range(N_VERT):
@@ -125,3 +207,79 @@ func init_labels():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+func gen_ans_sub(ix : int, line_used):
+	#print_cells()
+	#print_box_used()
+	var x : int = ix % N_HORZ
+	if x == 0: line_used = 0
+	var x3 = x / 3
+	var y2 = ix / (N_HORZ*2)
+	var bix = y2 * 2 + x3
+	var used = line_used | column_used[x] | box_used[bix]
+	if used == ALL_BITS: return false		# 全数字が使用済み
+	var lst = []
+	var mask = BIT_1
+	for i in range(N_HORZ):
+		if (used & mask) == 0: lst.push_back(mask)		# 数字未使用の場合
+		mask <<= 1
+	if ix == N_CELLS - 1:
+		cell_bit[ix] = lst[0]
+		return true
+	if lst.size() > 1: lst.shuffle()
+	for i in range(lst.size()):
+		cell_bit[ix] = lst[i]
+		column_used[x] |= lst[i]
+		box_used[bix] |= lst[i]
+		if gen_ans_sub(ix+1, line_used | lst[i]): return true
+		column_used[x] &= ~lst[i]
+		box_used[bix] &= ~lst[i]
+	cell_bit[ix] = 0
+	return false;
+func gen_ans():		# 解答生成
+	print("gen_ans():")
+	for i in range(N_CELLS):
+		#clue_labels[i].text = "?"
+		input_labels[i].text = ""
+	for i in range(box_used.size()): box_used[i] = 0
+	for i in range(cell_bit.size()): cell_bit[i] = 0
+	var t = []
+	for i in range(N_HORZ): t.push_back(1<<i)
+	t.shuffle()
+	for i in range(N_HORZ):
+		cell_bit[i] = t[i]
+		column_used[i] = t[i]
+		box_used[i/3] |= t[i]
+	#print(cell_bit)
+	gen_ans_sub(N_HORZ, 0)
+	#print_cells()
+	#update_cell_labels()
+	#ans_bit = cell_bit.duplicate()
+	#for i in range(N_CELLS): ans_bit[i] = cell_bit[i]
+	for i in range(N_CELLS): ans_num[i] = bit_to_num(cell_bit[i])
+	#print_ans()
+	#print_ans_num()
+	for i in range(N_CELLS): input_labels[i].text = ""		# 入力ラベル全消去
+	#for i in range(N_CELLS): input_labels[i].text = bit_to_numstr(cell_bit[i])
+	print_cells()
+	pass
+func print_cells():
+	var ix = 0
+	for y in range(N_VERT):
+		var lst = []
+		for x in range(N_HORZ):
+			var n = bit_to_num(cell_bit[ix])
+			input_labels[ix].text = "%d" % n
+			lst.push_back(n)
+			ix += 1
+		print(lst)
+	print("")
+func print_ans_num():
+	print("ans_num:")
+	var ix = 0
+	for y in range(N_VERT):
+		var lst = []
+		for x in range(N_HORZ):
+			lst.push_back(ans_num[ix])
+			ix += 1
+		print(lst)
+	print("")
