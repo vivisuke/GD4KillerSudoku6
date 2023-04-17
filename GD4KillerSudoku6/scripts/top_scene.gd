97a8442ff1ccb2433b7b68d499d2a6ffa37fb86f
vivisuke <ntsuda@master.email.ne.jp>
2023-04-17 11:32:33 +0900

    0091 ◎ C 04/17 04/17 0.001│ToDo│トップシーン：問題生成ボタン押下→メインシーンに遷移

---
+++ GD4KillerSudoku6/scripts/top_scene.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+const N_BUTTONS = 6
+
+var buttons = []
+#onready var g = get_node("/root/Global")
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	pass
+
+func to_MainScene(qLevel):
+	print($LineEdit.text)
+	g.qLevel = qLevel
+	g.qName = $LineEdit.text
+	g.qRandom = $LineEdit.text == ""
+	g.qNumber = 0
+	g.todaysQuest = false
+	get_tree().change_scene_to_file("res://main_scene.tscn")
+func _on_button_0_pressed():
+	to_MainScene(0)
+func _on_button_1_pressed():
+	to_MainScene(1)
+func _on_button_2_pressed():
+	to_MainScene(2)
