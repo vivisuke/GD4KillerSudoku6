f285b01b6e687b0439e5894eaf6a546afb819f0f
vivisuke <ntsuda@master.email.ne.jp>
2023-04-20 06:01:49 +0900

    0123 ◎ C 04/20 04/20 0.001│ToDo│トップシーン→レベルシーン遷移

--- GD4KillerSudoku6/scripts/top_scene.gd
+++ GD4KillerSudoku6/scripts/top_scene.gd
@@ -29,6 +29,19 @@ func _on_button_1_pressed():
 func _on_button_2_pressed():
 	to_MainScene(2)
 
-
+func to_LevelScene(qLevel):
+	#print($LineEdit.text)
+	g.qNumber = 0
+	g.qLevel = qLevel
+	g.qName = ""
+	g.qRandom = false
+	g.todaysQuest = false
+	get_tree().change_scene_to_file("res://level_scene.tscn")
+func _on_button_3_pressed():
+	to_LevelScene(0)
+func _on_button_4_pressed():
+	to_LevelScene(1)
+func _on_button_5_pressed():
+	to_LevelScene(2)
 func _on_button_6_pressed():
 	get_tree().change_scene_to_file("res://todays_quest.tscn")
